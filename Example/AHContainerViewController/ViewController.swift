655cbbe5a38b2e1c65096c96f316e36962c46ad8
Alex Hmelevski <alexei.hmelevski@gmail.com>
2017-07-12 20:58:49 -0400

    Initial commit

---
+++ Example/AHContainerViewController/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AHContainerViewController
+//
+//  Created by AlexHmelevskiAG on 07/12/2017.
+//  Copyright (c) 2017 AlexHmelevskiAG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
