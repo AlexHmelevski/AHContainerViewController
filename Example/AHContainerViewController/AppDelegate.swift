173d2c829405898f888670cc79a3ebcd22bc84fc
Alex Hmelevski <alexei.hmelevski@gmail.com>
2017-07-25 10:41:04 -0400

    new

--- Example/AHContainerViewController/AppDelegate.swift
+++ Example/AHContainerViewController/AppDelegate.swift
@@ -18,31 +18,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window?.rootViewController = container
         window?.makeKeyAndVisible()
-//
-//        let proportion = SizeProportion(width:0.6, height: 1)
-//
-//        var slidUpPopover = defaultProviderFactory.provider(for: .slideRight(size: proportion),
-//                                                            dimmingViewType: .defaultBlur)
-//
-//
-//        slidUpPopover.dimmingTapHandler = {[weak self] in
-//            guard let `self` = self else {return}
-//            self.container.pop(using: self.defaultProviderFactory.provider(for: .slideLeft(size: SizeProportion(width: 1, height: 1)),
-//                                                                           dimmingViewType: .noDimming),
-//                               completion: nil)
-//        }
-//        let newVC = UIViewController()
-//        newVC.view.backgroundColor = .red
-//        container.show(newVC, using: defaultProviderFactory.provider(for: .slideUp(size: SizeProportion.equal), dimmingViewType: .noDimming), completion: {(done) in
-//            let vc2 = UIViewController()
-//            vc2.view.backgroundColor = .yellow
-//            self.container.push(controller: vc2,
-//                                with: slidUpPopover,
-//                                completion: { (done) in
-//
-//            })
-//
-//        })
+        
+        let proportion = SizeProportion(width:0.6, height: 1)
+        
+        var slidUpPopover = defaultProviderFactory.provider(for: .slideRight(size: proportion),
+                                                            dimmingViewType: .defaultBlur)
+        
+        
+        slidUpPopover.dimmingTapHandler = {[weak self] in
+            guard let `self` = self else {return}
+            self.container.pop(using: self.defaultProviderFactory.provider(for: .slideLeft(size: SizeProportion(width: 1, height: 1)),
+                                                                           dimmingViewType: .noDimming),
+                               completion: nil)
+        }
+        let newVC = UIViewController()
+        newVC.view.backgroundColor = .red
+        container.show(newVC, using: defaultProviderFactory.provider(for: .slideUp(size: SizeProportion.equal), dimmingViewType: .noDimming), completion: {(done) in
+            let vc2 = UIViewController()
+            vc2.view.backgroundColor = .yellow
+            self.container.push(controller: vc2,
+                                with: slidUpPopover,
+                                completion: { (done) in
+                                    
+            })
+            
+        })
         return true
     }
 
